814789a11e8c63785df1a20084cc37904a6e1b36
daisyc620 <117552985+daisyc620@users.noreply.github.com>
2022-11-05 22:37:47 -0400

    Add files via upload

---
+++ tests/test-daisy-function.py
@@ -0,0 +1,18 @@
+from HW1_CURR import main
+
+def test():
+    # # A dictionary defining the set of currency pairs we will be pulling data for
+    # currency_pairs = [["AUD","USD",[],portfolio("AUD","USD")],
+    #                 ["GBP","EUR",[],portfolio("GBP","EUR")],
+    #                 ["USD","CAD",[],portfolio("USD","CAD")],
+    #                 ["USD","JPY",[],portfolio("USD","JPY")],
+    #                 ["USD","MXN",[],portfolio("USD","MXN")],
+    #                 ["EUR","USD",[],portfolio("EUR","USD")],
+    #                 ["USD","CNY",[],portfolio("USD","CNY")],
+    #                 ["USD","CZK",[],portfolio("USD","CZK")],
+    #                 ["USD","PLN",[],portfolio("USD","PLN")],
+    #                 ["USD","INR",[],portfolio("USD","INR")]]
+
+    # # Run the main data collection loop
+    # main(currency_pairs)
+    # assert HW1_CURR.main()
